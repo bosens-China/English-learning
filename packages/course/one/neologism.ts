cd5889feef5aa849e56c4e44491cb8bdd2fc9068
yliu <yangboses@gmail.com>
2024-04-24 22:12:43 +0800

    feat: add lesson one

---
+++ packages/course/one/neologism.ts
@@ -0,0 +1,1 @@
+export default ['excuse', 'handbag', 'me', 'pardon', 'yes', 'it', 'is', 'thank you', 'this', 'very much', 'your'];
---
+++ packages/course/one/text.ts
@@ -0,0 +1,9 @@
+export default [
+  'Excuse me!',
+  'yes?',
+  'is this your handbag',
+  'Pardon?',
+  'is this your handbag',
+  'Yes,it is',
+  'Thank you very much',
+];
